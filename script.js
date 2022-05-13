2a53c28a7f97ec00c9d855554bbbb31f11478af1
Shuber <shuberinu@gmail.com>
2022-05-13 12:02:41 +0700

    toggle accordion

---
+++ script.js
@@ -0,0 +1,9 @@
+const accordionQuestions = document.querySelectorAll('.accordion-header');
+
+accordionQuestions.forEach((q) => {
+	q.addEventListener('click', () => {
+		q.closest('.accordion')
+			.querySelector('.accordion-body')
+			.classList.toggle('show');
+	});
+});
