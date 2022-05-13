8f89ac19f034c6ab2a7f647dd9970713cd40db9a
Shuber <shuberinu@gmail.com>
2022-05-13 12:18:19 +0700

    alternate accordions

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const accordionQuestions = document.querySelectorAll('.accordion-header');
+const accordionBodies = document.querySelectorAll('.accordion-body');
+const accordionIcons = document.querySelectorAll('.accordion-icon');
 
 accordionQuestions.forEach((q) => {
 	q.addEventListener('click', () => {
+		// Remove class from all accordions
+		accordionBodies.forEach((b) => b.classList.remove('show'));
+		accordionIcons.forEach((i) => i.classList.remove('rotate'));
+
+		// Traverse DOM for adding classes to targeted accordion
 		q.closest('.accordion')
 			.querySelector('.accordion-body')
-			.classList.toggle('show');
-
-		q.querySelector('.accordion-icon').classList.toggle('rotate');
+			.classList.add('show');
+		q.querySelector('.accordion-icon').classList.add('rotate');
 	});
 });
